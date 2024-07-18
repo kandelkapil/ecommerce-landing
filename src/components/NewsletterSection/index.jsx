484713c11d89ddaa0dce3d6b89d640b804c929cc
kandelkapi1 <kapil@portpro.io>
2024-07-18 21:39:34 +0545

    subscription validation added

--- src/components/NewsletterSection/index.jsx
+++ src/components/NewsletterSection/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SectionContainer,
   SectionWrapper,
@@ -13,27 +13,51 @@ import {
 import Long1 from "../../assets/long-1.png";
 import Long2 from "../../assets/long-2.png";
 
-const NewsletterSection = () => (
-  <SectionContainer>
-    <SectionWrapper>
-      <LeftImage src={Long1} alt="Newsletter decoration left" />
-      <ContentWrapper>
-        <NewsletterTitle>Subscribe To Our Newsletter</NewsletterTitle>
-        <NewsletterDescription>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque
-          duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices
-          sollicitudin.
-        </NewsletterDescription>
-        <EmailInput
-          type="email"
-          placeholder="michael@ymail.com"
-          aria-label="Email for newsletter"
-        />
-        <SubscribeButton>Subscribe Now</SubscribeButton>
-      </ContentWrapper>
-      <RightImage src={Long2} alt="Newsletter decoration right" />
-    </SectionWrapper>
-  </SectionContainer>
-);
+const NewsletterSection = () => {
+  const [email, setEmail] = useState("");
+
+  const handleInputChange = (e) => {
+    setEmail(e?.target?.value);
+  };
+
+  const handleSubscribe = () => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValid = emailRegex.test(email);
+
+    if (isValid) {
+      alert("Subscription successful! Welcome to our newsletter.");
+      setEmail("");
+    } else {
+      alert("Please enter a valid email");
+    }
+  };
+
+  return (
+    <SectionContainer>
+      <SectionWrapper>
+        <LeftImage src={Long1} alt="Newsletter decoration left" />
+        <ContentWrapper>
+          <NewsletterTitle>Subscribe To Our Newsletter</NewsletterTitle>
+          <NewsletterDescription>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque
+            duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices
+            sollicitudin.
+          </NewsletterDescription>
+          <EmailInput
+            value={email}
+            onChange={(e) => handleInputChange(e)}
+            type="email"
+            placeholder="michael@ymail.com"
+            aria-label="Email for newsletter"
+          />
+          <SubscribeButton onClick={handleSubscribe}>
+            Subscribe Now
+          </SubscribeButton>
+        </ContentWrapper>
+        <RightImage src={Long2} alt="Newsletter decoration right" />
+      </SectionWrapper>
+    </SectionContainer>
+  );
+};
 
 export default NewsletterSection;
